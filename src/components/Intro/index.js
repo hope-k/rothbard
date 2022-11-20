d1b2812bf7e9863255bb36a53c02df083d670e35
hope-k <hopekumordzie@gmail.com>
2022-11-20 20:51:32 +0000

    rm aniamtions

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -60,45 +60,45 @@ const Intro = () => {
     return (
         <div className=' px-10 lg:px-[10rem] py-20  w-full  bg-slate-100'>
             <div className='bg-white flex justify-between rounded-2xl shadow-2xl border border-gray-200 lg:flex-row flex-col'  >
-                <animated.div onMouseLeave={() => setContainerHovered(false)} onMouseEnter={() => setContainerHovered(true)} style={containerAnimation} className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
-                    <animated.div style={svgAnimation} className='w-[5rem] mb-4'>
+                <div className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
+                    <div  className='w-[5rem] mb-4'>
                         <PiggyBank />
-                    </animated.div>
+                    </div>
                     <h1 className='block font-semibold text-[1.4rem] mb-4'> Saving Benefits</h1>
                     <span className='text-[14px] text-[#6084a4]'>
                         When you save with our savings accounts you get intrest on your money monthly..
                     </span>
-                </animated.div>
-                <animated.div style={containerAnimation} className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
-                    <animated.div style={svgAnimation} className='w-[5rem] mb-4'>
+                </div>
+                <div  className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
+                    <div  className='w-[5rem] mb-4'>
                         <Encrypt />
-                    </animated.div>
+                    </div>
                     <h1 className='block font-semibold text-lg text-[1.4rem] mb-4'>Fully Encrypted
                     </h1>
                     <span className='text-[14px] text-[#6084a4]'>
                         Our platform is properly encrypted and all transactions are interception free.
                     </span>
-                </animated.div>
-                <animated.div style={containerAnimation} className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
-                    <animated.div style={svgAnimation} className='w-[5rem] mb-4'>
+                </div>
+                <div  className='m-3 border-r border-gray-200 flex flex-col justify-center items-center p-5'>
+                    <div style={svgAnimation} className='w-[5rem] mb-4'>
                         <Pay />
-                    </animated.div>
+                    </div>
                     <h1 className='block font-semibold text-lg text-[1.4rem] mb-4 whitespace-nowrap'>Modern Payment Options
                     </h1>
                     <span className='text-[14px] text-[#6084a4]'>
                         Pay using modern payment options provided by our bank.
                     </span>
-                </animated.div>
-                <animated.div style={containerAnimation} className='m-3 flex flex-col justify-center items-center p-5'>
-                    <animated.div style={svgAnimation} className='w-[5rem] mb-4'>
+                </div>
+                <div style={containerAnimation} className='m-3 flex flex-col justify-center items-center p-5'>
+                    <div className='w-[5rem] mb-4'>
                         <Secure />
-                    </animated.div>
+                    </div>
                     <h1 className='block font-semibold text-lg text-[1.4rem] mb-4'>Safe and Secure
                     </h1>
                     <span className='text-[14px] text-[#6084a4]'>
                         Every account in our bank is secured to world class standards.
                     </span>
-                </animated.div>
+                </div>
 
             </div>
 

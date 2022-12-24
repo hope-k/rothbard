c504bc48d5eb659d43ba780369f8e9eeed7c6b50
hope-k <hopekumordzie@gmail.com>
2022-12-24 22:34:46 +0000

    minor ui changes.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -106,16 +106,15 @@ const LoginPage = () => {
                             </label>
 
                         </div>
-                        <button disabled={loading && true} type='submit' className='bg-[#1c2c5e] duration-200 text-[white] p-4  rounded-lg mb-4 disabled:bg-[#1c2d5e8e] flex justify-center items-center'>
+                        <button disabled={loading && true} type='submit' className='bg-[#1c2c5e] duration-200 text-[white] p-4 h-full w-full  rounded-lg mb-4 disabled:bg-[#1c2d5e8e] flex justify-center items-center relative'>
                             {
                                 loading ? 
-                                <div>
-                                    <RotatingLines
-                                        strokeColor="white"
-                                        strokeWidth="1.5"
-                                        animationDuration="0.75"
-                                        width="30"
-                                        
+                                <div className='p-4 relative bottom-2'>
+                                        <FadeLoader
+                                        width={1}
+                                        height={4}
+                                        margin={-7}
+                                            color='white'
                                         />
                                 
                                 </div> :
